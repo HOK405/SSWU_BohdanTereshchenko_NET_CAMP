bdbdc2c6227be50d4c215be2cb8b1fbbc4ba5bfc
hok <boghdantereshenko@gmail.com>
2023-04-03 23:53:28 +0300

    Task4_Tensor added

---
+++ Home_task_1/Task4_Tensor/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Linq.Expressions;
+
+namespace Task4_Tensor
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Enter number the number of dimensions:");
+            string? dimensionInput = Console.ReadLine();
+            int dimensionAmount = 0;
+            bool IsIntDimension = int.TryParse(dimensionInput, out dimensionAmount) && dimensionAmount > 0;
+
+            int[] sizes = new int[0];
+            int size = 0;
+
+            if (IsIntDimension)
+            {
+                sizes = new int[dimensionAmount];
+
+                for (int i = 0; i < dimensionAmount; i++)
+                {
+                    Console.WriteLine($"Enter size for dimension {i + 1}:");
+                    string? sizeInput = Console.ReadLine();
+                    
+                    bool IsIntSize = int.TryParse(sizeInput, out size) && size > 0;
+                    if (IsIntSize)
+                    {
+                        sizes[i] = size;
+                    }
+                }
+            }
+
+            Tensor myTensor = new Tensor();
+
+            try
+            {
+                myTensor = new Tensor(sizes);               
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.WriteLine($"Tensor with {myTensor.DimensionsAmount} dimensions is created.");
+        }
+    }
+}
---
+++ Home_task_1/Task4_Tensor/Tensor.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4_Tensor
+{
+    internal class Tensor
+    {
+        private int[] _sizes;
+        private Array? _tensor;
+
+        public Tensor(int[] sizes)
+        {
+            if (sizes != null)
+            {
+                for (int i = 0; i < sizes.Length; i++)
+                {
+                    if (sizes[i] <= 0)
+                    {
+                        throw new ArgumentOutOfRangeException("The value must be greater than zero");
+                    }
+                }
+            }
+            else
+            {
+                throw new NullReferenceException("An array mustn't be null.");
+            }
+
+            _sizes = sizes;
+            _tensor = FormTensor(sizes);
+        }
+
+        public Tensor()
+        {
+            _sizes = new int[0];
+            _tensor = default;
+        }
+
+        public int DimensionsAmount
+        {
+            get { return _sizes.Length; }
+        }
+
+        private Array FormTensor(int[] sizes, int currentIndex = 0)
+        {
+            int currentSize = sizes[currentIndex];
+            currentIndex++;
+
+            if (currentIndex == sizes.Length)
+            {
+                return Array.CreateInstance(typeof(int), currentSize);
+            }
+
+            Array array = Array.CreateInstance(typeof(Array), currentSize);
+            for (int i = 0; i < currentSize; i++)
+            {
+                array.SetValue(FormTensor(sizes, currentIndex), i);
+            }
+
+            return array;
+        }
+    }
+}
