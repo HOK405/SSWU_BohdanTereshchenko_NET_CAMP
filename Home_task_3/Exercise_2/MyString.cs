10480de50d078ca530fa02c0fb965cf921cb05ec
hok <boghdantereshenko@gmail.com>
2023-04-04 17:09:57 +0300

    Exercise_2

---
+++ Home_task_3/Exercise_2/MyString.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_2
+{
+    internal class MyString
+    {
+        private string _text;
+
+        public MyString(string text)
+        {
+            _text = text;
+        }
+
+        public string Data
+        {
+            get { return _text; }
+            set 
+            { 
+                if (value != null)
+                {
+                    _text = value;
+                }
+            }
+        }
+
+        public string? ReplaceDoubleLetterWords(string replacementText)
+        {
+            if (replacementText != null)
+            {
+                string[] words = _text.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                for (int i = 0; i < words.Length; i++)
+                {
+                    if (HasDoubleLetter(words[i]))
+                    {
+                        words[i] = replacementText;
+                    }
+                }
+                return string.Join(' ', words);
+            }
+            else
+                return null;
+        }
+
+        static bool HasDoubleLetter(string word)
+        {
+            for (int i = 0; i < word.Length - 1; i++)
+            {
+                if (word[i] == word[i + 1])
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public int? FindSecondEntry(string subString) // a) Find the index of the second occurrence of the substring
+        {
+            int firstIndex = _text.IndexOf(subString);
+
+            if (firstIndex == -1)
+            {
+                return null;
+            }
+            else
+            {
+                int secondIndex = _text.IndexOf(subString, firstIndex + 1);
+                if (secondIndex == -1)
+                {
+                    return null;
+                }
+                else
+                {
+                    return secondIndex;
+                }
+            }
+
+        }
+
+        public int CountWordsStartingWithUppercase(string text)
+        {
+            int count = 0;
+            string[] words = text.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            for (int i = 0; i < words.Length; i++)
+            {
+               if (!string.IsNullOrEmpty(words[i]) && char.IsUpper(words[i][0]))
+               {
+                   count++;
+               }
+            }
+            return count;
+        }
+    }
+}
---
+++ Home_task_3/Exercise_2/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Exercise_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputText = "The quick brown fox jjumps over the lazy dog. The lazy dog jumps over the quick brown fox. Dog";
+            string subString = "the";
+            string replacementText = "jumped";
+
+            MyString myString = new MyString(inputText);
+
+            // a) Find the index of the second occurrence of the substring
+            int? secondIndex = myString.FindSecondEntry(subString);
+            Console.WriteLine($"Index of the second occurrence of the substring: {secondIndex}");
+
+
+            // b) Count the number of words that start with an uppercase letter
+            int count = myString.CountWordsStartingWithUppercase(inputText);
+            Console.WriteLine($"Number of words starting with uppercase letter: {count}");
+
+
+            // c) Replace all words containing a double letter with the replacement text        
+            Console.WriteLine($"Text with replaced words:\n{myString.ReplaceDoubleLetterWords(replacementText)}");
+        }
+    }
+}
