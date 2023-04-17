ff1ef8fd2aa0b11ba0f329355d9063b2d6712f9f
hok <boghdantereshenko@gmail.com>
2023-04-17 16:52:10 +0300

    Task4 Exercise1

---
+++ Home_task_4/Exercise_1/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace Exercise_1
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+
+            List<string> textInEnglish = new List<string>()
+            {
+                "This is a sentence!",
+                "This is the second sentence ",
+                "with parentheses inside (It is the text inside parentheses), ",
+                "so it is the sample for my program.This ",
+                "is the third sentence ",
+                "with parentheses in the end (we are here).",
+                "This is the fourth sentence without parentheses.",
+                "(Also you can start your sentence with parentheses) And it is an ",
+                "example of it."
+            };
+
+            List<string> textInUkrainian = new List<string>()
+            {
+                "Це речення!",
+                "Це друге речення ",
+                "з дужками всередині (це текст всередині дужок), ",
+                "тому це приклад для моєї програми.Це ",
+                "третє речення ",
+                "з дужками всередині у кінці (ми тут).",
+                "Це четверте речення без дужок.",
+                "(Також ви можете починати ваше речення з дужками) І це цьому ",
+                "приклад."
+            };
+
+
+            List<string> englishParanthesis = SentenceSeparator.GetSentencesWithParentheses(textInEnglish);
+            List<string> ukrainianParanthesis = SentenceSeparator.GetSentencesWithParentheses(textInUkrainian);
+            
+            foreach (string sentance in englishParanthesis)
+                Console.WriteLine(sentance);
+
+            Console.WriteLine();
+
+            foreach (string sentance in ukrainianParanthesis)
+                Console.WriteLine(sentance);
+        }
+    }
+}
---
+++ Home_task_4/Exercise_1/SentenceSeparator.cs
@@ -0,0 +1,42 @@
+﻿using System.Text;
+
+namespace Exercise_1
+{
+    internal static class SentenceSeparator
+    {
+        public static List<string> GetSentencesWithParentheses(List<string> text)
+        {
+            List<string> _separatedSentences = new List<string>();
+            StringBuilder _sentence = new StringBuilder();
+
+            foreach (string line in text)
+            {
+                foreach (char c in line)
+                {
+                    if (c == '.' || c == '!' || c == '?')
+                    {
+                        _sentence.Append(c);
+                        _separatedSentences.Add(_sentence.ToString());
+                        _sentence.Clear();
+                    }
+                    else
+                    {
+                        _sentence.Append(c);
+                    }
+                }
+            }
+
+            List<string> _sentencesWithBrackets = new List<string>();
+
+            foreach (string s in _separatedSentences)
+            {
+                if (s.Contains("(") && s.Contains(")"))
+                {
+                    _sentencesWithBrackets.Add(s.ToString());
+                }
+            }
+
+            return _sentencesWithBrackets;
+        }
+    }
+}
