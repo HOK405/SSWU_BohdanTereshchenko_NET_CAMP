faf81b8c51e5576851c7a2ed767a773c143a6515
hok <boghdantereshenko@gmail.com>
2023-04-17 21:14:52 +0300

    Task4 Exercise2

---
+++ Home_task_4/Exercise_2/EmailsFinder.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Exercise_2
+{
+    internal class EmailsFinder
+    {
+        const int MAX_PARENTHESIS_AMOUNT = 1;
+        private string _text;
+
+        private List<string> _emails;
+        private List<string> _nonEmails;
+
+        public EmailsFinder(string text)
+        {
+            _text = text;
+            _emails = new List<string>();
+            _nonEmails = new List<string>();
+        }
+
+        public EmailsFinder()
+        {
+            _text = "";
+            _emails = new List<string>();
+            _nonEmails = new List<string>();
+        }
+        public List<string> GetEmails()
+        {
+            return _emails;
+        }
+
+        public List<string> GetNonEmails()
+        {
+            return _nonEmails;
+        }
+
+        public void DivideEmails()
+        {
+            string[] words = _text.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            foreach (string word in words)
+            {
+                if (word.Contains("@"))
+                {
+                    int atIndex = word.IndexOf("@");
+                    if (atIndex > 0 && atIndex < word.Length - 1)
+                    {
+                        string localPart = word.Substring(0, atIndex);
+                        string domainPart = word.Substring(atIndex + 1);
+                        if (IsValidLocalPart(localPart) && IsValidDomainPart(domainPart))
+                        {
+                            _emails.Add(word);
+                        }
+                        else
+                        {
+                            _nonEmails.Add(word);
+                        }
+                    }
+                    else
+                    {
+                        _nonEmails.Add(word);
+                    }
+                }
+            }
+        }
+
+        private bool IsValidLocalPart(string localPart)
+        {
+            // Перевірка частини локальної частини електронної адреси
+            if (string.IsNullOrEmpty(localPart))
+            {
+                return false;
+            }
+
+            // Перевірка на допустимі символи
+            string allowedCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789.!#$%&’*+/=?^_`{|}~-()";
+            foreach (char c in localPart)
+            {
+                if (!allowedCharacters.Contains(c))
+                {
+                    return false;
+                }
+            }
+
+            // Перевірка на довжину
+            if (localPart.Length > 64)
+            {
+                return false;
+            }
+
+            // Перевірка на крайні крапки
+            if (localPart.StartsWith(".") || localPart.EndsWith("."))
+            {
+                return false;
+            }
+
+            // Перевірка на послідовні крапки
+            if (localPart.Contains(".."))
+            {
+                return false;
+            }
+
+            // Перевірка на коментарі у дужках
+            if (!AreCommentsValid(localPart))
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        private bool IsValidDomainPart(string domainPart)
+        {
+            // Перевірка частини домену електронної адреси
+            if (string.IsNullOrEmpty(domainPart))
+            {
+                return false;
+            }
+
+            // Перевірка на допустимі символи
+            string allowedCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-()";
+            foreach (char c in domainPart)
+            {
+                if (!allowedCharacters.Contains(c) && c != '.')
+                {
+                    return false;
+                }
+            }
+
+            // Перевірка на довжину
+            if (domainPart.Length > 64)
+            {
+                return false;
+            }
+
+            // Перевірка на крайні крапки та дефіси
+            if (domainPart.StartsWith(".") || domainPart.EndsWith(".") || domainPart.StartsWith("-") || domainPart.EndsWith("-"))
+            {
+                return false;
+            }
+
+            // Перевірка на послідовні крапки
+            if (domainPart.Contains(".."))
+            {
+                return false;
+            }
+
+            // Перевірка на наявність TLD
+            string[] parts = domainPart.Split('.');
+            if (parts.Length < 2)
+            {
+                return false;
+            }
+
+            // Перевірка на довжину TLD
+            string tld = parts[parts.Length - 1];
+            if (tld.Length < 2)
+            {
+                return false;
+            }
+
+            // Перевірка на коментарі у дужках
+            if (!AreCommentsValid(domainPart))
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        private bool AreCommentsValid(string input)
+        {
+            int leftCounter = input.Count(f => (f == '('));
+            int rightCounter = input.Count(f => (f == ')'));
+
+            if (leftCounter > 1 || rightCounter > 1)
+            {
+                return false;
+            }
+
+            int startIndex = input.IndexOf('(');
+            while (startIndex != -1)
+            {
+                int endIndex = input.IndexOf(')', startIndex);
+                if (endIndex == -1)
+                {
+                    return false;
+                }
+                if (endIndex == startIndex + 1)
+                {
+                    return false;
+                }
+                startIndex = input.IndexOf('(', endIndex);
+            }
+            return true;
+        }
+    }
+}
---
+++ Home_task_4/Exercise_2/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace Exercise_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            
+            List<string> correctEmails = new List<string>();
+
+            correctEmails.Add("hello(aa)world@gmail.com");
+            correctEmails.Add("example@example.com");
+            correctEmails.Add("correct*address{one}@address.com(comment)");
+            correctEmails.Add("$correct!address@(comment)post.com");
+
+            List<string> nonCorectEmails = new List<string>();
+            nonCorectEmails.Add("example@.com");
+            nonCorectEmails.Add("hello((World@gmail.com");
+            nonCorectEmails.Add(".noncorrect@gmail.com");
+
+            List<string> result = correctEmails.Concat(nonCorectEmails).ToList();
+
+            string text = $"Правильні та неправильні ел. адреси: {string.Join(" ", result)} в одному тексті";
+            Console.WriteLine(text);
+            Console.WriteLine();
+
+            EmailsFinder emailsFinder = new EmailsFinder(text);
+            emailsFinder.DivideEmails();
+
+            Console.WriteLine("Правильні електронні адреси:");
+            foreach (string email in emailsFinder.GetEmails())
+            {
+                Console.WriteLine(email);
+            }
+
+            Console.WriteLine("\nЛексеми з символом @, але не є правильними електронними адресами:");
+            foreach (string nonEmail in emailsFinder.GetNonEmails())
+            {
+                Console.WriteLine(nonEmail);
+            }
+        }           
+    }
+}
